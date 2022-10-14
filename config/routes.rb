58fc56612346f866d073e8fccc3a8e16ba680599
Koima-Sam <sam17koima@gmail.com>
2022-10-14 16:43:20 +0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, only: [:index,:create]
+  resources :products, only: [:index,:create,:update,:destroy]
+  get '/me', to: 'users#show'
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
 end
